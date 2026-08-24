cc1af398789da7db2551e94d118e8e0b7c1fb551
MANISH SINGH <142906807+Codermanish75@users.noreply.github.com>
2026-08-24 20:00:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solve(self,arr,i,j,dp):
+        if i>=j:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        mini = float('inf')
+        for k in range(i,j):
+            temp=self.solve(arr,i,k,dp)+self.solve(arr,k+1,j,dp)+arr[i-1]*arr[k]*arr[j]
+            
+            mini=min(mini,temp)
+        
+        dp[i][j] = mini
+
+        return dp[i][j]
+
+            
+            
+            
+            
+    def matrixMultiplication(self, arr):
+        # code here
+        n=len(arr)
+        dp = [[-1] * n for _ in range(n)]
+        return self.solve(arr,1,n-1,dp)
+        
